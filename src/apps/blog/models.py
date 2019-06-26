e013dd3c890002d321049a265f0d765b061e0a74
vedun <vedun.mail@gmail.com>
2019-06-26 15:46:13 +0300

    pep8

--- src/apps/blog/models.py
+++ src/apps/blog/models.py
@@ -4,10 +4,15 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, models.CASCADE, verbose_name=_('Post author'))
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, models.CASCADE,
+        verbose_name=_('Post author'),
+    )
     title = models.CharField(verbose_name=_('Post title'), max_length=4096)
     body = models.TextField(verbose_name=_('Post body'))
-    creation_date = models.DateTimeField(verbose_name=_('Creation date'), auto_now_add=True)
+    creation_date = models.DateTimeField(
+        verbose_name=_('Creation date'), auto_now_add=True,
+    )
 
     class Meta:
         verbose_name = _('Post')

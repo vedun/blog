8592eef25bc08adc2b10f3be22db284219a2c521
vedun <vedun.mail@gmail.com>
2019-06-26 21:29:00 +0300

    список подписок для пользователя

--- src/apps/user/models.py
+++ src/apps/user/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    subscriptions = models.ManyToManyField('self', symmetrical=False)

59636771d2d55d1e2b42c4a47923f21bf3b70c58
vedun <vedun.mail@gmail.com>
2019-06-27 21:55:42 +0300

    для извлечения подписчиков при рассылке email

--- src/apps/user/models.py
+++ src/apps/user/models.py
@@ -4,4 +4,6 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    subscriptions = models.ManyToManyField('self', symmetrical=False)
+    subscriptions = models.ManyToManyField(
+        'self', symmetrical=False, related_name='subscribers'
+    )
